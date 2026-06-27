c574cd1c770249dc2335be2594b44abcf39c5240
EFIMOV KONSTANTIN <kostyaefimov2012@gmail.com>
2026-06-27 21:07:33 +0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices: return 0
+        minimum = prices[0]
+        profit = 0
+        for i in prices:
+            if i < minimum:
+                minimum = i
+            elif (i - minimum) > profit:
+                profit = i - minimum
+        return profit
